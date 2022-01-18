aa114a87660130a7686407388f2c352d53459347
QuentinGeerts <quentin.geerts@bstorm.be>
2022-01-18 11:27:47 +0100

    WAD21

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Demo01Component } from './demo/demo01/demo01.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+  // { path: "demo/demo01", component: Demo01Component },
+  { path: "demo", children: [
+    { path: "demo01", component: Demo01Component },
+  ] },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { Demo01Component } from './demo/demo01/demo01.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    Demo01Component
   ],
   imports: [
     BrowserModule,
